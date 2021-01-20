0654a881b814a6f03aca8a7e473ebb69099059d7
SDONER <serpil_4984@hotmail.com>
2021-01-21 01:09:55 +0300

    GenericsIntro - Demo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> liste = new MyList<string>();
+           // Console.WriteLine(liste.Count);
+            liste.Add("Serpil");
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
